a3412dcfafb9dc1704208e090eb78fe443c0ba6d
Duke Nukem <anon@ymous.org>
2023-12-05 20:21:56 +0100

    lets try gh pages

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,22 @@
+.. PowrUsr Docs documentation master file, created by
+   sphinx-quickstart on Tue Dec  5 16:35:12 2023.
+   
+   
+
+Welcome to PowrUsr Docs's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   bash
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PowrUsr Docs'
+copyright = '2023, PowrUsr'
+author = 'PowrUsr'
+
+version = '2023-12-05'
+release = 'alpha'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.githubpages',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'python_docs_theme'
+html_static_path = ['_static']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
